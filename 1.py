363abdb669efba24a85fdd28ef1e6c80cd64a75c
Z2ppbWVuZXpkZXph <62645524+Z2ppbWVuZXpkZXph@users.noreply.github.com>
2020-11-13 16:19:24 -0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("clear")
+
+print ("           =======================")
+print ("         ||        LOGING         ||")
+print ("           =======================")
+
+print ("Inicie sesion")
+
+username = input("Username: ")
+contraseña = input("Password: ")
+
+if username == "hfreanzr" and contraseña == "pbagenfrñn":
+        print ("FELICIDADES, DESCUBRISTE LA CONTRASEÑA")
+
+else:
+        print ("Vuelve a intentar")
